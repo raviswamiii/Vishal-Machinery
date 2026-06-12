0b43895822dbe60fa329e8083cfe659bce7ecd1a
raviswamiii <raviswamisamsung@gmail.com>
2026-06-12 11:19:09 +0530

    OurMachineSectionResponsivenessDone

--- Client/src/components/OurMachines.tsx
+++ Client/src/components/OurMachines.tsx
@@ -5,8 +5,8 @@ const products = [1, 2, 3];
 
 export const OurMachines = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-8 px-4 py-10">
-      <div className="flex flex-col gap-3">
+    <div className="bg-[#F6FAEF] min-h-screen flex flex-col gap-8 px-[4vw] sm:px-[8vw] py-10 md:relative">
+      <div className="flex flex-col gap-3 md:w-[60%] lg:w-[50%] xl:w-[40%]">
         <div className="flex items-center gap-2">
           <p className="text-yellow-400 text-md font-semibold montserrat">
             OUR MACHINES
@@ -23,30 +23,36 @@ export const OurMachines = () => {
         </p>
       </div>
 
-      {products.map((_, index) => (
-        <div key={index} className="flex items-center rounded-xl shadow-lg">
-          <img
-            className="h-55"
-            src={PackagingMachine}
-            alt="Packaging Machines"
-          />
-          <div className="">
-            <h1 className="text-md font-bold montserrat mb-2">
-              SEMI AUTOMATIC POUCH PACKING MACHINE
-            </h1>
-            <p className="text-sm text-gray-800 mb-2">
-              Cost-effective and efficient solution for small to medium
-              production.
-            </p>
-            <button className="text-yellow-400 text-sm montserrat font-semibold">
-              VIEW DETAILS
-              <GoArrowRight className="text-lg inline-block ml-4" />
-            </button>
-          </div>
-        </div>
-      ))}
+      <div className=" flex flex-col md:flex-row gap-[2vw]">
+        {products.map((_, index) => (
+          <div
+            key={index}
+            className="bg-white flex md:flex-col items-center rounded-xl shadow-lg"
+          >
+            <img
+              className="h-55"
+              src={PackagingMachine}
+              alt="Packaging Machines"
+            />
 
-      <button className="montserrat border border-gray-600 font-bold text-sm py-4 rounded-sm">
+            <div className="px-[2vw] md:pb-[2vw] lg:px-[4vw]">
+              <h1 className="text-md font-bold montserrat mb-2">
+                SEMI AUTOMATIC POUCH PACKING MACHINE
+              </h1>
+              <p className="text-sm text-gray-800 mb-2">
+                Cost-effective and efficient solution for small to medium
+                production.
+              </p>
+              <button className="text-yellow-400 text-sm montserrat font-semibold">
+                VIEW DETAILS
+                <GoArrowRight className="text-lg inline-block ml-4" />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button className="montserrat border border-gray-600 font-bold text-sm py-4 md:px-4 rounded-sm md:absolute md:right-[10vw] md:top-[21vh]">
         VIEW ALL MACHINES
         <GoArrowRight className="text-lg inline-block ml-4" />
       </button>
