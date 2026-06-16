7b3829bfeb331f9e1833b006c18156d280e24bca
raviswamiii <raviswamisamsung@gmail.com>
2026-06-16 20:35:45 +0530

    FooterBugFix

--- Client/src/components/Footer.tsx
+++ Client/src/components/Footer.tsx
@@ -23,65 +23,71 @@ const pages = [
 
 export const Footer = () => {
   return (
-    <div className="bg-black min-h-screen p-4 text-white">
-      <div className="flex flex-col gap-6 border-b-[0.5px] border-gray-300/50 py-6">
-        <div className="flex items-center gap-8">
-          <TfiHeadphoneAlt className="text-5xl text-yellow-500" />
-          <div>
-            <p className="text-md font-semibold montserrat">
-              NEED HELP CHOOSING THE RIGHT MACHINE?
-            </p>
-            <p className="text-sm text-gray-400">Talk to our experts today.</p>
-          </div>
-        </div>
-
-        <button className="bg-yellow-400 flex items-center justify-center text-black text-sm font-bold py-4 rounded-lg montserrat">
-          CONTACT US
-          <GoArrowRight className="ml-6 text-lg" />
-        </button>
-      </div>
-
-      <div className="py-6 flex flex-col gap-4">
-        <div className="flex items-center gap-2">
-          <img
-            className="h-10 w-auto rounded-full overflow-hidden"
-            src={logo}
-            alt="Vishal Machinery Logo"
-          />
-          <div>
-            <h1 className="text-2xl tracking-widest bold-bebas-neue h-6">
-              VISHAL
-            </h1>
-            <p className="text-[10px] tracking-[0.2em] bebas-neue">MACHINERY</p>
-          </div>
-        </div>
-
-        <p className="text-sm text-gray-400">
-          High performance packaging machines <br /> built for precision,
-          reliability and efficiency.
-        </p>
-
-        <div className="flex gap-4 mt-2">
-          {socialMediaIcons.map((socialMediaIcon, index) => (
-            <div key={index} className="border rounded-full p-3">
-              <socialMediaIcon.icon />
+    <div className="relative">
+      <div className="bg-black min-h-screen text-white pt-10 px-[4vw] sm:px-[8vw]">
+        <div className="flex flex-col gap-6 border-b-[0.5px] border-gray-300/50 pb-6">
+          <div className="flex items-center gap-8">
+            <TfiHeadphoneAlt className="text-5xl text-yellow-500" />
+            <div>
+              <p className="text-md font-semibold montserrat">
+                NEED HELP CHOOSING THE RIGHT MACHINE?
+              </p>
+              <p className="text-sm text-gray-400">
+                Talk to our experts today.
+              </p>
             </div>
-          ))}
+          </div>
+
+          <button className="bg-yellow-400 flex items-center justify-center text-black text-sm font-bold py-4 rounded-lg montserrat w-full">
+            CONTACT US
+            <GoArrowRight className="ml-6 text-lg" />
+          </button>
         </div>
 
-        <div>
-          {pages.map((page, index) => (
-            <p
-              key={index}
-              className="py-3 text-sm text-gray-300 font-semibold border-b-[0.5px] border-gray-300/50 montserrat"
-            >
-              {page.page}
-            </p>
-          ))}
+        <div className="pt-6 flex flex-col gap-4">
+          <div className="flex items-center gap-2">
+            <img
+              className="h-10 w-auto rounded-full overflow-hidden"
+              src={logo}
+              alt="Vishal Machinery Logo"
+            />
+            <div>
+              <h1 className="text-2xl tracking-widest bold-bebas-neue h-6">
+                VISHAL
+              </h1>
+              <p className="text-[10px] tracking-[0.2em] bebas-neue">
+                MACHINERY
+              </p>
+            </div>
+          </div>
+
+          <p className="text-sm text-gray-400">
+            High performance packaging machines <br /> built for precision,
+            reliability and efficiency.
+          </p>
+
+          <div className="flex gap-4 mt-2">
+            {socialMediaIcons.map((socialMediaIcon, index) => (
+              <div key={index} className="border rounded-full p-3">
+                <socialMediaIcon.icon />
+              </div>
+            ))}
+          </div>
+
+          <div>
+            {pages.map((page, index) => (
+              <p
+                key={index}
+                className="py-3 text-sm text-gray-300 font-semibold border-b-[0.5px] border-gray-300/50 montserrat"
+              >
+                {page.page}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className="flex items-center text-xs gap-1 text-gray-500 justify-center ">
+      <div className="flex items-center text-xs gap-1 text-gray-500 justify-center absolute bottom-5 w-full">
         <MdOutlineCopyright />
         <p>Vishal Machinery. All rights reserved.</p>
       </div>
