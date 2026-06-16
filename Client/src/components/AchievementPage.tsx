f6fda8d0badea5ca61d8e7ab25cfd01686cea8c1
raviswamiii <raviswamisamsung@gmail.com>
2026-06-16 18:34:49 +0530

    AchievementPageResponsivenessComplete

--- Client/src/components/AchievementPage.tsx
+++ Client/src/components/AchievementPage.tsx
@@ -12,14 +12,14 @@ const achievementData = [
 
 export const AchievementPage = () => {
   return (
-    <div className="flex flex-col gap-4 p-4">
+    <div className="flex flex-col md:flex-row md:justify-between px-[4vw] sm:px-[8vw] md:py-10 h-full">
       {achievementData.map((achievement, index) => (
         <div
           key={index}
-          className="flex items-center gap-4 h-full py-4 border-b-[0.5px] border-gray-700/50"
+          className="flex items-center md:justify-center gap-4 flex-1 md:w-full py-[4vh] md:px-4 border-b-[0.5px] md:border-b-0 md:border-r-[0.5px] border-gray-700/50 last:border-0"
         >
           <achievement.icon className="text-4xl" />
-          <div className="">
+          <div>
             <p className="text-lg font-bold montserrat">{achievement.number}</p>
             <p className="text-xs text-gray-700 font-bold montserrat">
               {achievement.label}
