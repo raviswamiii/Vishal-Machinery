01cbea9753b8ae5975c897ee9fadf106d7552050
raviswamiii <raviswamisamsung@gmail.com>
2026-07-08 18:01:09 +0530

    fixingHomeHeroSection

--- Client/src/components/Hero.tsx
+++ Client/src/components/Hero.tsx
@@ -14,7 +14,70 @@ const bottomBar = [
 
 export const Hero = () => {
   return (
-    <div className="h-[calc(100vh-60px)] bg-black">
+    <div className="h-[calc(100vh-60px)] max-h-187.5 flex flex-col">
+      <div className="flex flex-col justify-center overflow-hidden">
+        <div className="border border-red-500 flex-1 absolute z-10 w-[50%] sm:w-[45%] lg:w-[40%] text-white pl-[4vw] sm:pl-[8vw] flex flex-col justify-center gap-[2vh]">
+          <div className="flex flex-col gap-[1vh]">
+            <p className="font-semibold bg-linear-to-r from-yellow-300 via-yellow-500 to-amber-700 bg-clip-text text-transparent">
+              BUILD FOR PRECISION.
+            </p>
+
+            <h1 className="text-[7vh] sm:text-[8vh] md:text-[9vh] lg:text-[10vh] font-semibold bebas-neue leading-none">
+              BUILD FOR <br />{" "}
+              <span className="text-yellow-400">PERFORMANCE.</span>
+            </h1>
+          </div>
+
+          <p className="text-xs sm:text-sm text-gray-300">
+            High-performance industrial machines engineered for reliability,
+            precision, and unmatched efficiency—helping businesses improve
+            productivity and achieve consistent results across every operation.
+          </p>
+
+          <div className="flex flex-col gap-[2vh] mt-[1vh]">
+            <button className="bg-yellow-400 text-black text-xs font-extrabold rounded-sm flex justify-center items-center gap-2 py-[2.5vh] px-[2vw] sm:w-[75%] md:w-[65%]">
+              <RiCameraLensFill className="text-xl" />
+              <span>EXPLORE MACHINES</span>
+            </button>
+
+            <button className="bg-black text-white border border-gray-100 text-xs font-bold rounded-sm flex justify-center items-center gap-2 py-[2.5vh] px-[2vw] sm:w-[75%] md:w-[65%]">
+              <MdOutlineSlowMotionVideo className="text-xl" />
+              <span>WATCH VIDEO</span>
+            </button>
+          </div>
+        </div>
+
+        <div className="border-5 border-amber-500 h-[calc(100vh-140px)] overflow-hidden relative">
+          <img
+            className="h-full w-full"
+            src={BackgroundImg}
+            alt="Packaging Machine"
+          />
+          <img
+            className="h-full w-full object-cover md:object-contain absolute top-0 -right-30 sm:-right-35 md:-right-45 lg:-right-55"
+            src={PackagingMachine}
+            alt="Packaging Machine"
+          />
+        </div>
+      </div>
+
+      <div className="h-20 border-t border-white/20 text-white col-span-2 flex items-center">
+        {bottomBar.map((items, index) => (
+          <div
+            key={index}
+            className="border-r border-white/20 last:border-r-0 w-full flex items-center justify-center gap-2 text-xs px-2"
+          >
+            <items.icon className="text-4xl" />
+            <p>{items.lable}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+{
+  /* <div className="h-[calc(100vh-60px)] max-h-187.5 bg-black">
       <div className="h-[calc(100vh-140px)] absolute z-10 w-[50%] text-white pl-[4vw] sm:pl-[8vw] flex flex-col justify-center gap-4">
         <div className="flex flex-col gap-2">
           <p className="font-semibold bg-linear-to-r from-yellow-300 via-yellow-500 to-amber-700 bg-clip-text text-transparent">
@@ -57,7 +120,7 @@ export const Hero = () => {
           alt="Packaging Machine"
         />
       </div>
-
+      
       <div className="h-20 border-t border-white/20 text-white col-span-2 flex items-center">
         {bottomBar.map((items, index) => (
           <div
@@ -69,6 +132,5 @@ export const Hero = () => {
           </div>
         ))}
       </div>
-    </div>
-  );
-};
+    </div> */
+}
