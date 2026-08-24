8639269905fcfe52e87514300acf0c3329bb92f5
raviswamiii <raviswamisamsung@gmail.com>
2026-08-24 15:49:37 +0530

    CretedAddItemsPageInFrontend

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,10 +1,15 @@
 import { Dashboard } from "../src/Pages/Dashboard";
 import { AddItem } from "../src/components/AddItem";
 import { Route, Routes } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Dashboard />
       <Routes>
         <Route path="/addItem" element={<AddItem />} />
--- admin/src/components/AddItem.tsx
+++ admin/src/components/AddItem.tsx
@@ -1,6 +1,155 @@
+import { useState } from "react";
+import axios from "axios";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
+import upload_area from "../assets/upload_area.png";
 
 export const AddItem = () => {
+  const [image1, setImage1] = useState<File | null>(null);
+  const [image2, setImage2] = useState<File | null>(null);
+  const [image3, setImage3] = useState<File | null>(null);
+  const [image4, setImage4] = useState<File | null>(null);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  const onSubmitHandler = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("price", price);
+
+      image1 && formData.append("image1", image1);
+      image2 && formData.append("image2", image2);
+      image3 && formData.append("image3", image3);
+      image4 && formData.append("image4", image4);
+
+      const response = await axios.post(
+        backendUrl + "/api/product/add",
+        formData,
+        // { headers: { token } },
+      );
+      console.log(response.data);
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        setName("");
+        setDescription("");
+        setImage1(null);
+        setImage2(null);
+        setImage3(null);
+        setImage4(null);
+        setPrice("");
+      }
+    } catch (error) {
+      console.log(error);
+
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data?.message || "Something went wrong");
+      } else if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
+  };
+
   return (
-    <div>AddItem</div>
-  )
-}
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col sm:w-full items-start gap-3 p-4 sm:px-16 sm:py-8"
+    >
+      <div>
+        <p className="mb-2">Upload Image</p>
+        <div className="flex gap-2">
+          <label htmlFor="image1">
+            <img
+              className="sm:w-20 w-16 cursor-pointer"
+              src={!image1 ? upload_area : URL.createObjectURL(image1)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage1(e.target.files?.[0] || null)}
+              type="file"
+              id="image1"
+              hidden
+            />
+          </label>
+          <label htmlFor="image2">
+            <img
+              className="sm:w-20 w-16 cursor-pointer"
+              src={!image2 ? upload_area : URL.createObjectURL(image2)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage2(e.target.files?.[0] || null)}
+              type="file"
+              id="image2"
+              hidden
+            />
+          </label>
+          <label htmlFor="image3">
+            <img
+              className="sm:w-20 w-16 cursor-pointer"
+              src={!image3 ? upload_area : URL.createObjectURL(image3)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage3(e.target.files?.[0] || null)}
+              type="file"
+              id="image3"
+              hidden
+            />
+          </label>
+          <label htmlFor="image4">
+            <img
+              className="sm:w-20 w-16 cursor-pointer"
+              src={!image4 ? upload_area : URL.createObjectURL(image4)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage4(e.target.files?.[0] || null)}
+              type="file"
+              id="image4"
+              hidden
+            />
+          </label>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2">Product name</p>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          className="w-full max-w-125 px-3 py-2"
+          type="text"
+          placeholder="Type here"
+          required
+        />
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2">Product description</p>
+        <textarea
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className="w-full max-w-125 px-3 py-2"
+          placeholder="Write content here"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-28 sm:py-3 py-2 sm:mt-4 mt-2 bg-black text-white"
+      >
+        ADD
+      </button>
+    </form>
+  );
+};
