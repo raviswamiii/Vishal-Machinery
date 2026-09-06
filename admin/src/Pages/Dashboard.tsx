8a1895b65d21d74b4bc467b05f52eee6f5fe3279
raviswamiii <raviswamisamsung@gmail.com>
2026-09-06 15:00:16 +0530

    fixedLogoSpelling

--- admin/src/Pages/Dashboard.tsx
+++ admin/src/Pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import { PiUserCirclePlusLight } from "react-icons/pi";
 import { Check, Plus } from "lucide-react";
 import { AddItem } from "../components/AddItem";
