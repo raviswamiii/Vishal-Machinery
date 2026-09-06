62ddc8e21d046d4024ea9587ec0745f1dea09b1f
raviswamiii <raviswamisamsung@gmail.com>
2026-09-06 11:01:14 +0530

    fixedDoubleRenderingOfAddItemPageOnListItemPage

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,9 +1,6 @@
 import { Dashboard } from "../src/Pages/Dashboard";
-import { AddItem } from "../src/components/AddItem";
-import { Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { ListItems } from "./components/ListItems";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -12,10 +9,6 @@ export const App = () => {
     <div>
       <ToastContainer />
       <Dashboard />
-      <Routes>
-        <Route path="/" element={<AddItem />} />
-        <Route path="/listItems" element={<ListItems />} />
-      </Routes>
     </div>
   );
 };
