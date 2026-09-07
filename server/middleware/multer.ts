c75935f31f053ba9486c10a989176e03d292e373
raviswamiii <raviswamisamsung@gmail.com>
2026-09-07 14:17:03 +0530

    changedMulter

--- server/middleware/multer.ts
+++ server/middleware/multer.ts
@@ -1,7 +1,11 @@
 import multer from "multer";
 
-const upload = multer({
-  dest: "uploads/",
-});
+const storage = multer.diskStorage({
+    filename:function(req,file,callback){
+        callback(null,file.originalname)
+    }
+})
 
-export default upload;
+const upload = multer({storage})
+
+export default upload
